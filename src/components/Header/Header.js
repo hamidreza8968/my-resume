9912a002576faf894b17e51f5f57ddddaeb597e0
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-08-21 12:23:19 +0200

    adding "Header component".

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,26 @@
 import React, {useState} from "react";
+import "./Header.css";
 
 export default function Header({myInformation}) {
-    const [stage , setStage] = useState("introduction");
-    const [stageNum , setStageNum] = useState(1);
+    const [stage, setStage] = useState("introduction");
+    const [stageNum, setStageNum] = useState(1);
 
     return (
-        <header className="header">
+        <header className="header container">
             <div className="header-profile">
                 <p className="profile-name">{myInformation.firstName} {myInformation.lastName}</p>
                 <p className="profile-job">{myInformation.job}</p>
             </div>
-            <div className="header-links">
-                <a className="link-cv" href="#">download my cv</a>
-                <a className="link-touch" href="#">get in touch</a>
-            </div>
-            <div className="header-divider">
-                <div className="divider-number">{stageNum}</div>
-                <p className="divider-text">{stage}</p>
-                <hr className="divider-line"/>
+            <div className="Header-right-box">
+                <div className="header-links">
+                    <a className="link" href="#">download my cv</a>
+                    <a className="btn" href="#">get in touch</a>
+                </div>
+                <div className="header-divider">
+                    <div className="divider-number">{stageNum}</div>
+                    <p className="divider-text">{stage}</p>
+                    <hr className="divider-line"/>
+                </div>
             </div>
         </header>
     )
