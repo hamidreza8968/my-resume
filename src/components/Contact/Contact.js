72ab262a1a705f8cf173f195680f9aacf31c550f
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-08-28 10:41:15 +0200

    edit form of "Contact" component.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,20 +3,25 @@ import Button from "../Button/Button";
 import {useState} from "react";
 
 export default function Contact() {
-    const [name,setName]=useState("");
-    const [email,setEmail]=useState("");
-    const [message,setMessage]=useState("");
+    const [form, setForm] = useState({name: "", email: "", message: ""});
+    const [isValidEmail, setIsValidEmail] = useState(true);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const validateEmail = (input) => {
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailPattern.test(input);
+    };
+
+    console.log(form);
 
     function handleSubmit(e) {
         e.preventDefault();
-        if (!name || !email || !message) return  alert("Please complete all the form's items!");
-
-        const newMessage = {Sender:name, Email:email, text:message};
+        setIsSubmitted(true);
+        if (!form.name || !form.email || !form.message) return;
+        const newMessage = {Sender: form.name, Email: form.email, text: form.message};
         console.log(newMessage);
-
-        setName("");
-        setEmail("");
-        setMessage("");
+        setForm({name: "", email: "", message: ""});
+        setIsSubmitted(false);
     }
 
     return (
@@ -26,15 +31,38 @@ export default function Contact() {
             <form className="form" onSubmit={handleSubmit} action="#">
                 <div className="form-item">
                     <label htmlFor="name">Name</label>
-                    <input className="input" value={name} onChange={(e) => setName(e.target.value)} type="text" id="name"/>
+                    <input className="input" value={form.name}
+                           onChange={(e) => setForm((prevState) => ({
+                               ...prevState, name: e.target.value,
+                           }))}
+                           type="text"
+                           id="name"/>
+
+                    {isSubmitted && !form.name && <p className="error-message">Please fill the fields!</p>}
                 </div>
                 <div className="form-item">
                     <label htmlFor="email">Email</label>
-                    <input className="input" value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email"/>
+                    <input className={isValidEmail ? "input" : "input invalid"} value={form.email} type="email" id="email"
+                           onChange={(e) => {
+                               setForm((prevState) => ({
+                                   ...prevState,
+                                   email: e.target.value,
+                               }));
+                               setIsValidEmail(validateEmail(e.target.value));
+                           }}/>
+                    {isSubmitted && !form.email && <p className="error-message">Please fill the fields!</p>}
+                    {!isValidEmail && form.email!=="" && <p className="error-message">Please enter a valid email address.</p>}
                 </div>
                 <div className="form-item">
                     <label htmlFor="message">Message</label>
-                    <textarea className="input" value={message} onChange={(e) => setMessage(e.target.value)} name="message" id="message" cols="40" rows="8" placeholder="Type your message..."></textarea>
+                    <textarea className="input" value={form.message}
+                              onChange={(e) => setForm((prevState) => ({
+                                  ...prevState,
+                                  message: e.target.value,
+                              }))}
+                              name="message" id="message" cols="40" rows="8"
+                              placeholder="Type your message..."/>
+                    {isSubmitted && !form.message && <p className="error-message">Please fill the fields!</p>}
                 </div>
                 <div className="form-item">
                     <Button handleClick={handleSubmit}>submit</Button>
