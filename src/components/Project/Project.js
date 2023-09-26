6c2c45549642d050a9c1e26801a55ca975982963
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-09-26 13:51:38 +0200

    WIP: add project list

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -10,13 +10,13 @@ export default function Project({projectObj}) {
         setImageSrc(projectObj.src);
     };
     return (
-        <div className="project-card">
+        <a className="project-card" href={projectObj.address} target="_blank">
             <img src={imageSrc} alt="picture of website" onMouseEnter={handleMouseEnter}
                  onMouseLeave={handleMouseLeave}/>
             <h1 className="project-title">{projectObj.title}</h1>
             <p>{projectObj.tools.map((tool, i) =>
                 <span className="project-tools" key={i}>{tool},&nbsp;</span>
             )}</p>
-        </div>
+        </a>
     )
 }
