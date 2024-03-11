fd460848dd3664880b41d0eb2f70a6a127701b16
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2024-03-11 08:12:42 +0100

    edit: edit mailsentry link

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -5,7 +5,7 @@ export default function Experience() {
 
 
     const experiences= [
-        {title: "Frontend Web Developer", company: "MailSentry.io",  start: "Apr 2023" , end: "Now"},
+        {title: "Frontend Web Developer", company: "MailSentry (https://mailsentry.io/)",  start: "Apr 2023" , end: "Now"},
         {title: "Frontend Web Developer", company: "Freelance(Self employed)",  start: "Aug 2022" , end: "Mar 2023"},
         {title: "System and Process specialist" , company: "Kowsar Insurance", start: "Mar 2017" , end: "Nov 2022"},
         ];
