d17f9146e906cbe0e9c0b2b75e89f5d1feec09c3
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2024-06-12 14:35:15 +0200

    edit: Experience component

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -5,8 +5,9 @@ export default function Experience() {
 
 
     const experiences= [
-        {title: "Frontend Web Developer", company: "MailSentry (https://mailsentry.io/)",  start: "Apr 2023" , end: "Now"},
-        {title: "Frontend Web Developer", company: "Freelance(Self employed)",  start: "Aug 2022" , end: "Mar 2023"},
+        {title: "Frontend Web Developer", company: "Innovatogy",  start: "Mar 2024" , end: "Now"},
+        {title: "Frontend Web Developer", company: "MailSentry",  start: "Mar 2023" , end: "Mar 2024"},
+        {title: "Frontend Web Developer", company: "Freelance",  start: "May 2022" , end: "Feb 2023"},
         {title: "System and Process specialist" , company: "Kowsar Insurance", start: "Mar 2017" , end: "Nov 2022"},
         ];
 
